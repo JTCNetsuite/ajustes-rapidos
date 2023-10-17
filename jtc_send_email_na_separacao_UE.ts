06b220a17e273caf7ea99adf0e2069c217039846
Guzman Rivero <guzmanrivero@Air-de-Guzman.jtc.local>
2023-10-17 15:01:10 -0300

    email sepracao email faturado

---
+++ jtc_send_email_na_separacao_UE.ts
@@ -0,0 +1,89 @@
+/**
+ * @NAPIVersion 2.x
+ * @NScriptType UserEventScript
+ */
+
+
+import { EntryPoints } from 'N/types'
+import * as log from 'N/log'
+import * as email from "N/email"
+import * as search from 'N/search'
+
+
+
+export const afterSubmit: EntryPoints.UserEvent.afterSubmit = (ctx: EntryPoints.UserEvent.afterSubmitContext) => {
+    try {
+        
+        const curr = ctx.newRecord
+        const idSalesOrd = curr.getValue("createdfrom")
+
+        log.debug("type", curr.type)
+        const status = curr.getValue("shipstatus")
+
+        const pedido_vendas =curr.getText("createdfrom")
+
+        const searchEmails = search.create({
+            type: search.Type.SALES_ORDER,
+            filters: [
+                ["internalid", search.Operator.ANYOF, idSalesOrd],
+                "AND",
+                ["mainline", search.Operator.IS, "T"]
+            ],
+            columns: [
+                search.createColumn({name: "tranid", label: "Número do documento"}),
+                search.createColumn({name: "entity", label: "Nome"}),
+                search.createColumn({name: "partner", label: "Parceiro"}),
+                search.createColumn({
+                    name: "email",
+                    join: "partner",
+                    label: "E-mail"
+                }),
+                search.createColumn({
+                    name: "email",
+                    join: "customer",
+                    label: "E-mail"
+                 })
+            ]
+        }).run().getRange({start: 0, end: 1})
+
+        if (searchEmails.length > 0) {
+            const recipients: any[] = [
+                searchEmails[0].getValue({name: 'email', join: 'partner'}),
+                searchEmails[0].getValue({name: 'email', join: 'customer'})
+            ]
+            log.debug("emails", recipients)
+
+            if (status == "B") {
+                try {
+                    email.send({
+                        author: -5,
+                        body: `Prezado cliente! <br></br>Informamos que o seu pedido da JTC Distribuidora foi SEPARADO`,
+                        subject: String(pedido_vendas),
+                        recipients: recipients
+                    })
+                } catch (error) {
+                    log.error("jtc_send_email_erro.SEPARADO", error)
+                }
+               
+            } else if (status == "C") {
+                // try {
+                //     email.send({
+                //         author: -5,
+                //         body: `Prezado cliente! <br></br>Informamos que o seu pedido da JTC Distribuidora foi ENVIADO`,
+                //         subject: String(pedido_vendas),
+                //         recipients: ['edison@jtcd.com.br']
+                //     })
+                // } catch (error) {
+                //     log.error("jtc_send_email_erro.ENVIADO", error)
+                // }
+            }
+        }
+
+        
+
+
+
+    } catch (error) {
+        log.error("jtc_send_email_na_separcao_UE.afterSubmit", error)
+    }
+}
--- jtc_send_email_para_extrema.ts
+++ jtc_send_email_para_extrema.ts
@@ -20,8 +20,10 @@ export const afterSubmit: EntryPoints.UserEvent.afterSubmit = (ctx: EntryPoints.
             const nf = curr.getValue('custbody_enl_fiscaldocnumber')
             const transportadora = curr.getValue('custbody_enl_carrierid')
             
+
+            
     
-            if (subsidiary == 7 || subsidiary == '7') {
+            // if (subsidiary == 7 || subsidiary == '7') {
                 if (!!nf) {
                     const link_nf = curr.getValue('custbody_enl_linknotafiscal')
                     const searchXML = search.create({
@@ -66,34 +68,101 @@ export const afterSubmit: EntryPoints.UserEvent.afterSubmit = (ctx: EntryPoints.
                         log.debug("idFile", idFile)
     
                         const fileXml = file.load({id: idFile})
-    
-                        const body = `Segue o anexo do Xml <br></br>
-                         <a href="${link_nf}">Link da nf</a>
-                        `
+
+                        
+                        if (subsidiary == "7" || subsidiary == 7) {
+                            const body = `Segue o anexo do Xml <br></br>
+                                <a href="${link_nf}">Link da nf</a>
+                                `
                        
-                        email.send({
-                            author: 4,
-                            body: body,
-                            subject: `NF ${nf}`,
-                            recipients: [email_trans],
-                            cc: ['denis@jtcd.com.br'],
-                            attachments: [fileXml]
-                        })
-    
-                        const invoice = record.load({type: record.Type.INVOICE, id: ctx.newRecord.id})
-    
-                        invoice.setValue({fieldId: 'custbody_jtc_envio_email_transportador', value: true})
-                        invoice.save({ignoreMandatoryFields: true})
+                            email.send({
+                                author: 4,
+                                body: body,
+                                subject: `NF ${nf}`,
+                                recipients: [email_trans],
+                                cc: ['denis@jtcd.com.br'],
+                                attachments: [fileXml]
+                            })
+        
+                            const invoice = record.load({type: record.Type.INVOICE, id: ctx.newRecord.id})
+        
+                            invoice.setValue({fieldId: 'custbody_jtc_envio_email_transportador', value: true})
+                            invoice.save({ignoreMandatoryFields: true})
+                        }
+
+                        enviarEmailParaCliente(ctx, link_nf, fileXml)
     
                     }
     
                     
                     
                 }
-            }
+            // }
         
 
     } catch (error) {
         log.error("jtc_send_email_para_extrema.afterSubmit", error)
+    }
+}
+
+
+const enviarEmailParaCliente = (ctx: EntryPoints.UserEvent.afterSubmitContext, link_nf, fileXml: file.File) => {
+    try {
+        const curr = ctx.newRecord
+        const idSalesOrd = curr.getValue("createdfrom")
+
+        log.debug("type", curr.type)
+        const status = curr.getValue("shipstatus")
+
+        const pedido_vendas =curr.getText("createdfrom")
+
+        const searchEmails = search.create({
+            type: search.Type.SALES_ORDER,
+            filters: [
+                ["internalid", search.Operator.ANYOF, idSalesOrd],
+                "AND",
+                ["mainline", search.Operator.IS, "T"]
+            ],
+            columns: [
+                search.createColumn({name: "tranid", label: "Número do documento"}),
+                search.createColumn({name: "entity", label: "Nome"}),
+                search.createColumn({name: "partner", label: "Parceiro"}),
+                search.createColumn({
+                    name: "email",
+                    join: "partner",
+                    label: "E-mail"
+                }),
+                search.createColumn({
+                    name: "email",
+                    join: "customer",
+                    label: "E-mail"
+                 })
+            ]
+        }).run().getRange({start: 0, end: 1})
+
+        if (searchEmails.length  > 0 ) {
+            const recipients: any[] = [
+                searchEmails[0].getValue({name: 'email', join: 'partner'}),
+                searchEmails[0].getValue({name: 'email', join: 'customer'})
+            ]
+            log.debug("recepientes",recipients)
+            try {
+                email.send({
+                    author: -5,
+                    body: `Prezado cliente! <br></br>Informamos que o seu pedido da JTC Distribuidora foi ENVIADO <br></br> <a href="${link_nf}">acesse sua nf clicando aqui!</a>`,
+                    subject: String(pedido_vendas),
+                    recipients: recipients,
+                    attachments: [fileXml]
+                })
+                log.audit("Email Enviado", "ENVIADO")
+            } catch (error) {
+                log.error("jtc_send_email_erro.ENVIADO", error)
+            }
+        }
+        
+
+
+    } catch (error) {
+        log.error("jtc_send_email_para_extrema.enviarEmailParaCliente", error)
     }
 }
