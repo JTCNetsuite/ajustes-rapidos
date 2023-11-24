5337fdd5bb568e3418cbee0a4e4e347ae31794c1
Guzman Rivero <guzmanrivero@Air-de-Guzman.jtc.local>
2023-11-24 11:00:31 -0300

    jtc_email_est_min

--- jtc_send_email_estoque_minimo_MR.ts
+++ jtc_send_email_estoque_minimo_MR.ts
@@ -9,15 +9,15 @@ import {EntryPoints } from 'N/types'
 import * as log from 'N/log'
 import * as search from 'N/search'
 import * as record from 'N/record'
-import * as file from 'N/file'
-
+import * as email from 'N/email'
 
 export const getInputData: EntryPoints.MapReduce.getInputData = () => {
 
     
 	try {
         return search.create({
-            type: search.Type.LOT_NUMBERED_INVENTORY_ITEM
+            type: search.Type.LOT_NUMBERED_INVENTORY_ITEM,
+            filters: ["internalid",  search.Operator.ANYOF, 7]
         })
     } catch (error) {
         log.error("jtc_send_email_estoque_minimo_MR.getInputData",error)
@@ -35,20 +35,7 @@ export const map: EntryPoints.MapReduce.map = (ctx: EntryPoints.MapReduce.mapCon
             type: record.Type.LOT_NUMBERED_INVENTORY_ITEM
         })
 
-        const getLine = itemRecord.getLineCount({sublistId: 'locations'})
-
-        for (var i =0; i < getLine; i++) {
-            const location = itemRecord.getSublistValue({
-                fieldId: 'location',
-                sublistId: 'locations',
-                line: i
-            })
-
-            if (location == 2 || location == 6) {
-                
-            }
-
-        }
+        itemRecord.save({ignoreMandatoryFields: true})
 
 
 
