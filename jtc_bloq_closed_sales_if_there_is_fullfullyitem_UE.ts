78a2057a43ef580d429f26201c2a7fce5b923604
Guzman Rivero <guzmanrivero@Air-de-Guzman.jtc.local>
2024-02-15 09:23:11 -0300

    jtc_bloq_closed_sale_if_there

--- jtc_bloq_closed_sales_if_there_is_fullfullyitem_UE.ts
+++ jtc_bloq_closed_sales_if_there_is_fullfullyitem_UE.ts
@@ -8,6 +8,7 @@ import { EntryPoints } from "N/types"
 import * as log from 'N/log'
 import * as search from "N/search"
 import * as runtime from 'N/runtime'
+import {FieldDisplayType} from 'N/ui/serverWidget'
 
 export const beforeSubmit: EntryPoints.UserEvent.beforeSubmit = (ctx: EntryPoints.UserEvent.beforeSubmitContext) => {
 
@@ -16,36 +17,50 @@ export const beforeSubmit: EntryPoints.UserEvent.beforeSubmit = (ctx: EntryPoint
 export const beforeLoad: EntryPoints.UserEvent.beforeLoad = (ctx: EntryPoints.UserEvent.beforeLoadContext) => {
 
     // const field = ctx.form.getField({id: 'closeremaining'})
-    if (ctx.type == ctx.UserEventType.VIEW) {
-        const curr = ctx.newRecord
+    // if (ctx.type == ctx.UserEventType.VIEW) {
+    //     const curr = ctx.newRecord
    
-        const searchAtendimento = search.create({
-            type: search.Type.ITEM_FULFILLMENT,
-            filters: [
-                ['mainline', search.Operator.IS, "T"],
-                    "AND",
-                ['createdfrom', search.Operator.ANYOF, curr.id]
-            ]
-        }).runPaged().count
+    //     const searchAtendimento = search.create({
+    //         type: search.Type.ITEM_FULFILLMENT,
+    //         filters: [
+    //             ['mainline', search.Operator.IS, "T"],
+    //                 "AND",
+    //             ['createdfrom', search.Operator.ANYOF, curr.id]
+    //         ]
+    //     }).runPaged().count
     
-        log.debug("searchAtenfimento", searchAtendimento)
+    //     log.debug("searchAtenfimento", searchAtendimento)
     
-        const status_inte = curr.getValue("custbody_jtc_integration_status")
+    //     const status_inte = curr.getValue("custbody_jtc_integration_status")
           
-        const currUser = runtime.getCurrentUser().role
+    //     const currUser = runtime.getCurrentUser().role
         
-        if (searchAtendimento > 0 ) {
-            ctx.form.removeButton({id: 'closeremaining'})
-        }
-        if  (status_inte == 1) {
-            if (currUser != 3 ) {
-                ctx.form.removeButton({id: 'closeremaining'})
-            }
-        }
-    }
+    //     if (searchAtendimento > 0 ) {
+    //         ctx.form.removeButton({id: 'closeremaining'})
+    //     }
+    //     if  (status_inte == 1) {
+    //         if (currUser != 3 ) {
+    //             ctx.form.removeButton({id: 'closeremaining'})
+    //         }
+    //     }
+    // }
     
+    if ( ctx.type == ctx.UserEventType.EDIT ) {
+        const currUser = runtime.getCurrentUser().role
+        const form = ctx.form
+
+        const field = form.getField({
+            id:'custbody_jtc_tip_exp'
+        })
+        if (currUser != 3) {
+            field.updateDisplayType({displayType: FieldDisplayType.DISABLED})
+
+        }
+
+
+    }
 
 
     // log.debug("fiedl", field)
 
-}
+}   
