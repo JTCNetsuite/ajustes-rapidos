caf0e01f545ed60992e3388cfd1d96c24650581a
Guzman Rivero <guzmanrivero@Air-de-Guzman.jtc.local>
2024-03-26 09:19:03 -0300

    block saleOrd

--- jtc_bloq_closed_sales_if_there_is_fullfullyitem_UE.ts
+++ jtc_bloq_closed_sales_if_there_is_fullfullyitem_UE.ts
@@ -10,40 +10,45 @@ import * as search from "N/search"
 import * as runtime from 'N/runtime'
 import {FieldDisplayType, FieldType} from 'N/ui/serverWidget'
 
-export const beforeSubmit: EntryPoints.UserEvent.beforeSubmit = (ctx: EntryPoints.UserEvent.beforeSubmitContext) => {
-
-}
 
 export const beforeLoad: EntryPoints.UserEvent.beforeLoad = (ctx: EntryPoints.UserEvent.beforeLoadContext) => {
 
-    // const field = ctx.form.getField({id: 'closeremaining'})
-    // if (ctx.type == ctx.UserEventType.VIEW) {
-    //     const curr = ctx.newRecord
+
+    if (ctx.type == ctx.UserEventType.VIEW) {
+        const curr = ctx.newRecord
    
-    //     const searchAtendimento = search.create({
-    //         type: search.Type.ITEM_FULFILLMENT,
-    //         filters: [
-    //             ['mainline', search.Operator.IS, "T"],
-    //                 "AND",
-    //             ['createdfrom', search.Operator.ANYOF, curr.id]
-    //         ]
-    //     }).runPaged().count
+        const searchAtendimento = search.create({
+            type: search.Type.ITEM_FULFILLMENT,
+            filters: [
+                ['mainline', search.Operator.IS, "T"],
+                    "AND",
+                ['createdfrom', search.Operator.ANYOF, curr.id]
+            ]
+        }).runPaged().count
+
+        const searchInvouice = search.create({
+            type: search.Type.INVOICE,
+            filters: [
+                ['mainline', search.Operator.IS, "T"],
+                    "AND",
+                ['createdfrom', search.Operator.ANYOF, curr.id]
+            ]
+        }).runPaged().count
     
-    //     log.debug("searchAtenfimento", searchAtendimento)
+        log.debug("searchAtenfimento", searchAtendimento)
+        log.debug("searchInvouice", searchInvouice)
     
-    //     const status_inte = curr.getValue("custbody_jtc_integration_status")
+        const status_inte = curr.getValue("custbody_jtc_integration_status")
           
-    //     const currUser = runtime.getCurrentUser().role
+        const currUser = runtime.getCurrentUser().role
         
-    //     if (searchAtendimento > 0 ) {
-    //         ctx.form.removeButton({id: 'closeremaining'})
-    //     }
-    //     if  (status_inte == 1) {
-    //         if (currUser != 3 ) {
-    //             ctx.form.removeButton({id: 'closeremaining'})
-    //         }
-    //     }
-    // }
+        if (searchAtendimento > 0 || searchInvouice > 0) {
+            if (currUser != 3) {
+                ctx.form.removeButton({id: 'closeremaining'})
+            }
+        }
+        
+    }
     
     if ( ctx.type == ctx.UserEventType.EDIT ) {
         const currUser = runtime.getCurrentUser().role
