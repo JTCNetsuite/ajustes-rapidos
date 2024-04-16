40333f8a9e76d0b722bddbc660244bb98df9cb21
Guzman Rivero <guzmanrivero@Air-de-Guzman.jtc.local>
2024-04-16 09:54:07 -0300

    nivel_preco

--- jtc_set_nivel_preco_CS.ts
+++ jtc_set_nivel_preco_CS.ts
@@ -19,153 +19,159 @@ export const postSourcing: EntryPoints.Client.postSourcing = (ctx:EntryPoints.Cl
         if (ctx.fieldId == 'item' && ctx.sublistId == 'item') {
             const clientID = curr.getValue("entity")
             
-            const clientData = search.lookupFields({
-                id: clientID,
-                type: search.Type.CUSTOMER,
-                columns: [
-                    "subsidiary","pricelevel",
-                    "Address.custrecord_enl_uf"
-                ]
-            })
-
-            const subClient = clientData.subsidiary[0].value
-            const priceClient = clientData.pricelevel[0].value
-            const ufClient = clientData['Address.custrecord_enl_uf'][0].text
-            const priceText = clientData.pricelevel[0].text
-            
-            // ** field custcoljtc_preco_tabela
-
-            console.log("clienteData", clientData)
-            console.log("uf", ufClient)
-            const sub = curr.getValue("subsidiary")
-
             const currentItem = curr.getCurrentSublistValue({
                 fieldId: 'item',
                 sublistId: 'item'
             })
-            const states_12_percent = ['MG', 'SP', 'PR', 'RS', 'RJ', 'SC']
 
-            let price = priceClient
-            if (sub != subClient) {
-               
-                // ** PEDIDO FORA DE SÃO PAULO(SP) OU EXTREMA(MG) PARA QUALQUER OUTRO ESTADO QUE SEJA DIFERENTE DE SP/MG
-                if ((sub == 7 || sub == 3) && (ufClient != 'SP' || ufClient != 'MG')) {
-                    price = priceClient
+            if (!!currentItem) {
+                const clientData = search.lookupFields({
+                    id: clientID,
+                    type: search.Type.CUSTOMER,
+                    columns: [
+                        "subsidiary","pricelevel",
+                        "Address.custrecord_enl_uf"
+                    ]
+                })
+    
+                const subClient = clientData.subsidiary[0].value
+                const priceClient = clientData.pricelevel[0].value
+                const ufClient = clientData['Address.custrecord_enl_uf'][0].text
+                const priceText = clientData.pricelevel[0].text
+                
+                // ** field custcoljtc_preco_tabela
+    
+                console.log("clienteData", clientData)
+                console.log("uf", ufClient)
+                const sub = curr.getValue("subsidiary")
+    
+                
+                const states_12_percent = ['MG', 'SP', 'PR', 'RS', 'RJ', 'SC']
+    
+                let price = priceClient
+                if (sub != subClient) {
                    
-                }
-
-                //** PEDIDOS DE SÃO PAULO(SP) PARA EXTREMA(MG) */
-                if (sub == 3 && ufClient == "MG" ) {
-                    console.log("pedidos de Sp para MG")
-                    if(priceClient == 1 || priceClient == 4) {
-                        price = 6
-                    } 
-
-                    if (priceClient == 2) {
-                        price = 10
+                    // ** PEDIDO FORA DE SÃO PAULO(SP) OU EXTREMA(MG) PARA QUALQUER OUTRO ESTADO QUE SEJA DIFERENTE DE SP/MG
+                    if ((sub == 7 || sub == 3) && (ufClient != 'SP' || ufClient != 'MG')) {
+                        price = priceClient
+                       
                     }
-                    if (priceClient == 5 || priceClient == 9) {
-                        price = 8
+    
+                    //** PEDIDOS DE SÃO PAULO(SP) PARA EXTREMA(MG) */
+                    if (sub == 3 && ufClient == "MG" ) {
+                        console.log("pedidos de Sp para MG")
+                        if(priceClient == 1 || priceClient == 4) {
+                            price = 6
+                        } 
+    
+                        if (priceClient == 2) {
+                            price = 10
+                        }
+                        if (priceClient == 5 || priceClient == 9) {
+                            price = 8
+                        }
                     }
-                }
-                //** PEDIDOS DE EXTREMA(MG) PARA EXTREMA(MG) */
-                if (sub == 7 && ufClient == "MG") {
-                    if (priceClient == 6) {
-                        price = 1
+                    //** PEDIDOS DE EXTREMA(MG) PARA EXTREMA(MG) */
+                    if (sub == 7 && ufClient == "MG") {
+                        if (priceClient == 6) {
+                            price = 1
+                        }
+    
+                        if (priceClient == 10 || priceClient == 7) {
+                            price = 2
+                        }
+    
+                        if (priceClient == 8) {
+                            price == 5
+                        }
                     }
-
-                    if (priceClient == 10 || priceClient == 7) {
-                        price = 2
+                    
+                    //** PEDIDOS DE EXTREMA(MG) PARA SÃO PAULO */
+                    if (sub == 7 && ufClient == "SP") {
+                        if(priceClient == 1 || priceClient == 4) {
+                            price = 6
+                        } 
+    
+                        if (priceClient == 2) {
+                            price = 10
+                        }
+                        if (priceClient == 5 || priceClient == 9) {
+                            price = 8
+                        }
                     }
-
-                    if (priceClient == 8) {
-                        price == 5
+                    
+                    //** PEDIDOS DE SÃO PAULO(SP) PARA SÃO PAULO(SP) */
+                    if (sub == 3 && ufClient == "SP") {
+                        if (priceClient == 6) {
+                            price = 1
+                        }
+    
+                        if (priceClient == 10 || priceClient == 7) {
+                            price = 2
+                        }
+    
+                        if (priceClient == 8) {
+                            price == 5
+                        }
                     }
+    
+                    if (sub == 5 && ufClient != 'PR') {
+                        if(priceClient == 1 || priceClient == 4) {
+                            price = 6
+                        } 
+    
+                        if (priceClient == 2) {
+                            price = 10
+                        }
+                        if (priceClient == 5 || priceClient == 9) {
+                            price = 8
+                        }
+                    }
+                    
+                    
+    
+    
+                    curr.setCurrentSublistValue({
+                        fieldId: 'price',
+                        sublistId: 'item',
+                        value: price
+                    })
                 }
                 
-                //** PEDIDOS DE EXTREMA(MG) PARA SÃO PAULO */
-                if (sub == 7 && ufClient == "SP") {
-                    if(priceClient == 1 || priceClient == 4) {
-                        price = 6
-                    } 
-
-                    if (priceClient == 2) {
-                        price = 10
-                    }
-                    if (priceClient == 5 || priceClient == 9) {
-                        price = 8
-                    }
-                }
-                
-                //** PEDIDOS DE SÃO PAULO(SP) PARA SÃO PAULO(SP) */
-                if (sub == 3 && ufClient == "SP") {
-                    if (priceClient == 6) {
-                        price = 1
-                    }
-
-                    if (priceClient == 10 || priceClient == 7) {
-                        price = 2
-                    }
-
-                    if (priceClient == 8) {
-                        price == 5
-                    }
-                }
-
-                if (sub == 5 && ufClient != 'PR') {
-                    if(priceClient == 1 || priceClient == 4) {
-                        price = 6
-                    } 
-
-                    if (priceClient == 2) {
-                        price = 10
-                    }
-                    if (priceClient == 5 || priceClient == 9) {
-                        price = 8
-                    }
-                }
-                
-                
-
-
+                console.log("price", price)
+                var inventoryitemSearchObj = search.create({
+                    type: "inventoryitem",
+                    filters:
+                    [
+                        ["type","anyof","InvtPart"], 
+                        "AND", 
+                        ["pricing.pricelevel","anyof", price], 
+                        "AND", 
+                        ["internalid","anyof",currentItem]
+                    ],
+                    columns:
+                    [
+                       search.createColumn({
+                          name: "unitprice",
+                          join: "pricing",
+                          label: "Preço unitário"
+                       }),
+                       search.createColumn({
+                          name: "pricelevel",
+                          join: "pricing",
+                          label: "Nível de preço"
+                       })
+                    ]
+                }).run().getRange({start: 0, end: 1})
+                console.log(inventoryitemSearchObj[0].getValue({name: 'unitprice', join:'pricing' }))
                 curr.setCurrentSublistValue({
-                    fieldId: 'price',
+                    fieldId: 'custcoljtc_preco_tabela',
                     sublistId: 'item',
-                    value: price
+                    value: inventoryitemSearchObj[0].getValue({name: 'unitprice', join:'pricing' })
                 })
             }
+
             
-            console.log("price", price)
-            var inventoryitemSearchObj = search.create({
-                type: "inventoryitem",
-                filters:
-                [
-                    ["type","anyof","InvtPart"], 
-                    "AND", 
-                    ["pricing.pricelevel","anyof", price], 
-                    "AND", 
-                    ["internalid","anyof",currentItem]
-                ],
-                columns:
-                [
-                   search.createColumn({
-                      name: "unitprice",
-                      join: "pricing",
-                      label: "Preço unitário"
-                   }),
-                   search.createColumn({
-                      name: "pricelevel",
-                      join: "pricing",
-                      label: "Nível de preço"
-                   })
-                ]
-            }).run().getRange({start: 0, end: 1})
-            console.log(inventoryitemSearchObj[0].getValue({name: 'unitprice', join:'pricing' }))
-            curr.setCurrentSublistValue({
-                fieldId: 'custcoljtc_preco_tabela',
-                sublistId: 'item',
-                value: inventoryitemSearchObj[0].getValue({name: 'unitprice', join:'pricing' })
-            })
         }
 
     } catch (error) {
